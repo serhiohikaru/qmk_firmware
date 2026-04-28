7aef2fdbf60c6cf061800c84335f33979662c94d
serhiohikaru <srgschein@gmail.com>
2026-04-28 17:12:00 +0200

    Change tapping term to 200

--- keyboards/crkbd/keymaps/serhiohikaru/config.h
+++ keyboards/crkbd/keymaps/serhiohikaru/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_DEFAULT_VAL 120
 
 // how long before a key press becomes a hold
-#define TAPPING_TERM 300
+#define TAPPING_TERM 200
 
 // value of the increment or decrement of the dynamic tapping term
 #define DYNAMIC_TAPPING_TERM_INCREMENT 50
